740f0d8b8c24f59b13f3e37323c031c3e4358eba
ooswaab <osabtel@outlook.jp>
2020-05-30 23:43:57 +0900

    2乗

--- Test.py
+++ Test.py
@@ -1,5 +1,8 @@
-sum = 0
+sum = 0  # 合計
+squ = 0  # 2乗の合計
 for i in range(1, 101):
-    print("表示", i)
+    print("表示", i, "2乗数：", i * i)
+    squ = squ + i * i
     sum = sum + i
 print("合計", sum)
+print("2乗値の合計", squ)
