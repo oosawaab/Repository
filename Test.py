504c0a1f74f0f6cff4a6e3c8af2d7588bdeb37d4
ooswaab <osabtel@outlook.jp>
2020-05-31 00:33:00 +0900

    偶数

--- Test.py
+++ Test.py
@@ -1,12 +1,16 @@
 sum = 0  # 合計
 squ = 0  # 2乗の合計
-kisusum = 0
+kisusum = 0  # 奇数の合計
+gususum = 0  # 偶数の合計
 for i in range(1, 101):
     #print("表示", i, "2乗数：", i * i)
     if i % 2 != 0:
         kisusum = kisusum + i
+    else:
+        gususum = gususum + i
     squ = squ + i * i
     sum = sum + i
 print("合計", sum)
 print("2乗値の合計", squ)
 print("奇数の合計:", kisusum)
+print("偶数の合計", gususum)
